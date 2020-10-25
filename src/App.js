f2b0b9e963119c2075008c5f601f0e92ef7ea60e
Astronomity <andrewliangsd@gmail.com>
2020-10-24 22:57:36 -0700

    Card framework added"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card } from './Card';
 
 function App() {
   return (
---
+++ src/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+/**
+ * TODO: Card function should have: 
+ * 1. Relevant link or picture
+ * 2. GitHub link if applicable
+ * 3. Description only if clicked
+ * 4. Animation to expand card to cover screen
+ * 4.a Expansion should be limited to card at the front of carousel.
+ * 5. Animation and button to revert card to see other projects
+ * 6. Easy to integrate into a carousel functionality
+ */
+export class Card extends React.Component {
+    /**
+     * Props will include Card title, Github enabled, imgsrc
+     * State expanded will be disabled at first
+     */
+    constructor(props) {
+        super(props);
+        this.state = {expanded: false};
+    }
+    toggleExpand() {
+        if(!this.state.expanded) {
+            // TODO: Expand state
+            this.state.expanded = true;
+        } else {
+            // TODO: Revert state
+            this.state.expanded = false;
+        }
+    }
+    render() {
+        return (
+            <div onClick={this.toggleExpand} >
+                <img /* Project image, specify this as a parameter for more modularity */ />
+                <img /* GitHub link if enabled within card (parametrize this too) */ />
+                /* Also make a back button that reverts to unexpanded visible on expand */
+            </div>
+        );
+    }
+}
