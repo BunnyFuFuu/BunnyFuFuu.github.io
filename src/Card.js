ab8b6cd21fa94c56697998f012d5bbe868a00f82
Astronomity <andrewliangsd@gmail.com>
2020-10-27 02:32:56 -0700

    Added Material UI

--- src/Card.js
+++ src/Card.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 /**
  * TODO: Card function should have: 
