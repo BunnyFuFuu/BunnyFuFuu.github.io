a2d816ae36dc9a9c19f5d79d64462750341ee5f2
Andrew Liang <aliang@ucsd.edu>
2020-12-15 21:57:03 -0800

    Updates

--- precache-manifest.048985524f8b34cb6b2dc1b3068b388e.js
+++ precache-manifest.048985524f8b34cb6b2dc1b3068b388e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5d788c6225974b2ed397498b94883855",
+    "revision": "0d1b2e965dd058a15e2f548e4fcfb8a7",
     "url": "/Personal-Website/index.html"
   },
   {
-    "revision": "6c2c62e588b58033c9a6",
+    "revision": "e305b46703e350f88f95",
     "url": "/Personal-Website/static/css/main.35a2c832.chunk.css"
   },
   {
-    "revision": "3b91c76262326c992adc",
-    "url": "/Personal-Website/static/js/2.170eaf22.chunk.js"
+    "revision": "6e2eda45ee62b8bdbaba",
+    "url": "/Personal-Website/static/js/2.a7061684.chunk.js"
   },
   {
     "revision": "3c73c585782ac05880c0f89bcfdbba5a",
-    "url": "/Personal-Website/static/js/2.170eaf22.chunk.js.LICENSE.txt"
+    "url": "/Personal-Website/static/js/2.a7061684.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6c2c62e588b58033c9a6",
-    "url": "/Personal-Website/static/js/main.2ba97d89.chunk.js"
+    "revision": "e305b46703e350f88f95",
+    "url": "/Personal-Website/static/js/main.4144a293.chunk.js"
   },
   {
     "revision": "1ddbfe5ccc0145171c3b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Personal-Website/precache-manifest.00c9f2fd5a8224984ea78953da73bf56.js"
+  "/Personal-Website/precache-manifest.048985524f8b34cb6b2dc1b3068b388e.js"
 );
 
 self.addEventListener('message', (event) => {
