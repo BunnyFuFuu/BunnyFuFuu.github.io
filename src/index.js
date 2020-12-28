e51a9f1bd50c0a8e5273e4aa8466b9121fad6f4e
Andrew Liang <aliang@ucsd.edu>
2020-12-27 23:55:21 -0800

    Auth0 wrapper

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Auth0Provider
+      domain="dev-j3ai5m4d.us.auth0.com"
+      clientId="ms7HTvXoZF3Q1LKkSJmoO6apcNcJACSV"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
