e267aace265f0f640d1bacc447a525ad7e1986be
Andrew Liang <aliang@ucsd.edu>
2020-12-27 23:56:07 -0800

    Added admin manage path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './auth/privateroute';
 import './App.css';
 import Introduction from './routes/introduction';
 import Profile from './routes/profile';
 import Experience from './routes/experience';
 import Projects from './routes/projects';
+import Manage from './routes/manage';
 
 const routes = [
-    { path: "/",           Component: Introduction },
-    { path: "/profile",    Component: Profile      },
-    { path: "/experience", Component: Experience   },
-    { path: "/projects",   Component: Projects     }
+    { path: "/",           Component: Introduction, private: false },
+    { path: "/profile",    Component: Profile     , private: false },
+    { path: "/experience", Component: Experience  , private: false },
+    { path: "/projects",   Component: Projects    , private: false },
+    { path: '/manage',     Component: Manage      , private: true  }
 ]
 
 export default () => {
@@ -19,7 +22,9 @@ export default () => {
             <div className="App">
                 <header className="App-header">
                     {
-                        routes.map(route => <Route exact path={route.path} component={route.Component}/>)
+                        routes.map(route => (route.private ?
+                            <PrivateRoute exact path={route.path} component={route.Component} /> :
+                            <Route exact path={route.path} component={route.Component} />))
                     }
                 </header>
             </div>
