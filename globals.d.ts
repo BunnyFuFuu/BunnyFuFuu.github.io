e77002f0d4211e0e4f5f6a0628ff46cc921fafef
Andrew Liang <aliang@ucsd.edu>
2021-01-01 16:11:22 -0800

    New document types added

--- globals.d.ts
+++ globals.d.ts
@@ -1,8 +1,25 @@
-declare type HobbyDocument= {
+declare type HobbyDocument = {
     title:  string;
     avatar: string;
     image:  string;
     blurb:  string;
     info:   string[]; // When processing management input, parse input box separated into paragraphs
     link:   string;
+}
+
+declare type ExperienceDocument = {
+    title:  string;
+    date:   string;
+    link?:  string;
+    blurb:  string;
+    info:   string[];
+}
+
+declare type ProjectDocument = {
+    title:  string; 
+    date:   string;
+    link?:  string;
+    status: string;
+    blurb:  string;
+    info:   string[];
 }
