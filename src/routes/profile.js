f8bf8b43fa6845e9c4d7232b7a4b57b199321f77
Andrew Liang <aliang@ucsd.edu>
2021-01-01 16:13:51 -0800

    Removed test card, waiting on backend

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,17 +2,7 @@ import React, { Component } from 'react';
 import { NavBar } from '../components/navbar';
 import Controller from '../controller/controller';
 import HobbyCard  from '../components/hobbycard';
-/**
- * @type HobbyDocument
- */
-const testDoc = {
-    title: "Sample Hobby",
-    avatar: "A",
-    image: "../assets/AV.jpg",
-    blurb: "Small sample blurb",
-    info: ["Yes"],
-    link: "yes"
-}
+
 export default class Profile extends Component {
     constructor(props) {
         super(props);
@@ -25,8 +15,7 @@ export default class Profile extends Component {
                 <div className="content">
                     <h1>About Me</h1>
                     <p>Here's a little more about my interests and hobbies!</p>
-                    {Controller.hobbies.map(i => <HobbyCard doc={i}></HobbyCard>)}
-                    <HobbyCard doc={testDoc}></HobbyCard>
+                    { Controller.hobbies.map(i => <HobbyCard doc={i}></HobbyCard>) }
                 </div>
             </div>
         )
