7c8224bcb42738353abbb74be1b8d366605c9c54
Andrew Liang <aliang@ucsd.edu>
2021-01-02 01:19:36 -0800

    Consolidated cards for modularity

--- globals.d.ts
+++ globals.d.ts
@@ -1,27 +1,13 @@
-declare type HobbyDocument = {
-    title:      string;
-    avatar:     string;
-    image:      string;
-    blurb:      string;
-    info:       string[]; // When processing management input, parse input box separated into paragraphs
-    link:       string;
-}
-
-declare type ExperienceDocument = {
-    title:      string;
-    date:       string;
-    image:      string;
-    link?:      string;
-    blurb:      string;
-    info:       string[];
-}
-
-declare type ProjectDocument = {
-    title:      string; 
-    date:       string;
-    image:      string;
-    link?:      string;
-    work:       string;
-    blurb:      string;
-    info:       string[];
+/**
+ * Modular document, meant to hold Hobby, Experience, and Project documents 
+ * (Functionally very similar but held in different db collections)
+ */
+declare type CardDocument = {
+    title: string;
+    date?: string;
+    image: string;
+    link?: string;
+    work:  string;
+    blurb: string;
+    info:  string[];
 }
