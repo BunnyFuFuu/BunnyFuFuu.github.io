b1bf06c36210c573ae9e6e6204385acf4b35433e
Andrew Liang <aliang@ucsd.edu>
2021-01-01 20:54:01 -0800

    Renamed picture to image for consistency

--- globals.d.ts
+++ globals.d.ts
@@ -1,25 +1,27 @@
 declare type HobbyDocument = {
-    title:  string;
-    avatar: string;
-    image:  string;
-    blurb:  string;
-    info:   string[]; // When processing management input, parse input box separated into paragraphs
-    link:   string;
+    title:      string;
+    avatar:     string;
+    image:      string;
+    blurb:      string;
+    info:       string[]; // When processing management input, parse input box separated into paragraphs
+    link:       string;
 }
 
 declare type ExperienceDocument = {
-    title:  string;
-    date:   string;
-    link?:  string;
-    blurb:  string;
-    info:   string[];
+    title:      string;
+    date:       string;
+    image:      string;
+    link?:      string;
+    blurb:      string;
+    info:       string[];
 }
 
 declare type ProjectDocument = {
-    title:  string; 
-    date:   string;
-    link?:  string;
-    status: string;
-    blurb:  string;
-    info:   string[];
+    title:      string; 
+    date:       string;
+    image:      string;
+    link?:      string;
+    work:       string;
+    blurb:      string;
+    info:       string[];
 }
