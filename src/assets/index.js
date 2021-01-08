cf9c8a0ed9b1380d9518d03e10c90097feac695d
Andrew Liang <aliang@ucsd.edu>
2021-01-08 00:35:19 -0800

    Background added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import PFP from "./pfp.jpg";
 import Resume from './Resume.pdf';
 import AV from "./AV.jpg";
+import BG from "./bg.jpg";
 
-export { PFP, Resume, AV };
+export { PFP, Resume, AV, BG };
